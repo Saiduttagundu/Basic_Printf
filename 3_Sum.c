8d3305c00cb5a5c5f15140b494bdc48be91c6eef
Saidutta Gundu <saidattagundu15@gmail.com>
2025-09-16 00:07:20 +0530

    Add files via upload

---
+++ 3_Sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    int num1, num2, sum;
+
+    printf("Enter Number 1:");
+    scanf("%d", &num1);
+
+    printf("Enter Number 2:");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("The sum of Numbers are:%d", sum);
+}
