933698ef93397b18ce58ed5b8824029ee029ae1f
Saidutta Gundu <saidattagundu15@gmail.com>
2025-09-17 23:38:03 +0530

    Add files via upload

---
+++ 6_Division.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    float i, j, division;
+
+    printf("Enter Number 1:");
+    scanf("%f", &i);
+
+    printf("Enter Number 2:");
+    scanf("%f", &j);
+
+    division = i / j;
+
+    printf("The Product of the numbers are: %f", division );
+}
