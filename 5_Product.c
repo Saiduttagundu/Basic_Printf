022cc4dbb38cbf9f84a84f11195202fbb4f6d01f
Saidutta Gundu <saidattagundu15@gmail.com>
2025-09-17 23:37:35 +0530

    Add files via upload

---
+++ 5_Product.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    int i, j, product;
+
+    printf("Enter Number 1:");
+    scanf("%d", &i);
+
+    printf("Enter Number 2:");
+    scanf("%d", &j);
+
+    product = i * j;
+
+    printf("The Product of the numbers are: %d", product );
+}
