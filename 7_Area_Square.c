99972c7e4cc15cf9dd52d784236f7cadd9e86af0
Saidutta Gundu <saidattagundu15@gmail.com>
2025-09-18 23:23:10 +0530

    Add files via upload

---
+++ 7_Area_Square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int a, area;
+
+    printf("Enter side:");
+    scanf("%d", &a);
+
+    area = a * a;
+
+    printf("Square of the given side is : %d",area);
+}
