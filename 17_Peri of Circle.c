58b850dfe59911a80bae47dcd9fddb81d34cc552
Saidutta Gundu <saidattagundu15@gmail.com>
2025-09-30 22:00:35 +0530

    Add files via upload

---
+++ 17_Peri of Circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() 
+{
+    float r, perimeter;
+
+    printf("Enter radius: ");
+    scanf("%f", &r);
+
+    perimeter = 2 * 3.14 * r;
+
+    printf("Perimeter = %.2f\n", 2 * 3.14 * r);
+    
+}
