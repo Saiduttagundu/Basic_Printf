b2bae2ac6cc3b4a18405d0ce7ab7304bc4ce3d57
Saidutta Gundu <saidattagundu15@gmail.com>
2025-10-02 15:36:55 +0530

    Add files via upload

---
+++ 19_Cube sq.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() 
+{
+    int n, cube;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    cube = n * n * n;
+
+    printf("Cube = %d\n", cube);
+    
+}
